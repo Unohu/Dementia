ab36166de5c572332141f3b714cf8bc17a6a2248
Jarskih <madcook86@gmail.com>
2020-02-26 14:56:14 +0100

    Scripts

---
+++ Assets/Scripts/Narrative.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (menuName = "Narrative")]
+public class Narrative : ScriptableObject
+{
+     public string id;
+     public Color color = Color.white;
+     public string line;
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> _waypoints = new List<Waypoint>();
+
+    private void Start()
+    {
+        int index = 0;
+        foreach (var w in _waypoints)
+        {
+            w.index = index;
+            index++;
+        }
+    }
+
+    public Waypoint GetNextWayPoint(int index)
+    {
+        if (index >= _waypoints.Count-1)
+        {
+            return _waypoints[0];
+        }
+
+        return _waypoints[index + 1];
+    }
+
+    public Waypoint GetFirstWaypoint()
+    {
+        return _waypoints[0];
+    }
+}
