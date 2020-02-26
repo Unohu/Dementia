c8e91ff70e8ccef3b6375b59e90bbb0c9452b4d9
Jarskih <madcook86@gmail.com>
2020-02-26 16:06:16 +0100

    Narrative

--- Assets/Scripts/Narrative.cs
+++ Assets/Scripts/Narrative.cs
@@ -6,5 +6,13 @@ public class Narrative : ScriptableObject
 {
      public string id;
      public Color color = Color.white;
+     [TextArea (minLines:7, maxLines: 7)]
      public string line;
+     public string person;
+
+     public string GetLine()
+     {
+          return person + ": " + line;
+
+     }
 }
---
+++ Assets/Scripts/NarrativeManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NarrativeManager : MonoBehaviour
+{
+    private List<Conversation> _conversations = new List<Conversation>();
+    private List<Narrative> _narratives = new List<Narrative>();
+    // Start is called before the first frame update
+         void Start()
+         {
+             
+         }
+     
+         // Update is called once per frame
+         void Update()
+         {
+             
+         }
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("FoundCoins", ShowText);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("FoundCoins", ShowText);
+    }
+
+    private void ShowText() {
+    }
+}
