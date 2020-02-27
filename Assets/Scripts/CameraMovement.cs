0cf2d94a79cf519b1004e8756986ec432b8dcbd2
Unohu <Audrius.Labanauskas.0746@student.uu.se>
2020-02-27 13:51:36 +0100

    Camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
