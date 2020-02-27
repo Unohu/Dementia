50d5a96aa1c9290f7dce20e05d11589bf47baf18
Unohu <Audrius.Labanauskas.0746@student.uu.se>
2020-02-27 12:18:21 +0100

    Player movement, interact scripts and placeholder animations..

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    Collider2D playerCollider;
+    void Start()
+    {
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Interactable" && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Picking Up Object");
+            Destroy(other.gameObject);
+        }
+    }
+}
